0214d43db50d094c52e790b1430eee677e7ab4de
Reeve <chihang.wong@edu.sait.ca>
2025-07-30 08:50:55 -0600

    update

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-import pandas
+import pandas as pd
 import streamlit as st
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-
-st.title('Random Forest Classifier')
+ 
+st.title("Penguine Speicy Prediction ML app")
+st.info("This is end-to-end Machine Learning App")
+ 
+with st.expander("Data"):
+  pass
+ 
+with st.expander("Data Visualization"):
+  pass
+ 
+with st.expander("Input data"):
+  pass
+ 
+with st.expander("Data Preperation"):
+  pass
+ 
+with st.sidebar:
+  st.header("Input Variables")
+  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
